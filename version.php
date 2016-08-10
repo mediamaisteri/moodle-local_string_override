ebdb25260c1857662bd691718d77b63db0569133
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2016-08-10 11:53:18 +0300

    Fixes Moodle version requirement to 2015051100 (v2.9.0)

--- version.php
+++ version.php
@@ -23,5 +23,5 @@
  */
 
 $plugin->version  = 2016061700;       // The (date) version of this plugin
-$plugin->requires = 2012120300;       // Requires this Moodle version
+$plugin->requires = 2015051100;       // Requires this Moodle version
 $plugin->component = 'local_string_override'; // Declares the type and name of this plugin
